1d99258bf9889b3d5f14f2b2a496206f15dbb100
spill002 <sreeja.pilli@uscellular.com>
2020-10-20 13:19:38 +0530

    q

--- node-typescript-example/src/models/todonum.ts
+++
@@ -1,5 +0,0 @@
-export interface TodoNum {
-    id: string;
-    text: number;
-  }
-  
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+const https = require('https');
+const fs = require('fs');
+
+
+const dbUrl1 = 'mongodb://localhost:27017/test';
+
+mongoose.connect(dbUrl1, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true });
+var db = mongoose.connection;
+
+mongoose.connection.on('connect', () => {
+    console.log('MongoDb connected');
+});
+
+db.on('error', console.error.bind(console, 'MongoDb connection Error!'));
+
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert')
+
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('API running')
+});
+
+app.use('/api/employees', require('./api/employees'));
+app.use('/api/dept', require('./api/dept'));
+
+https.createServer({ key: privateKey, cert: certificate }, app).listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`);
+})
